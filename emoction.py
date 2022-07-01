a349c95bd467e374a872a9113eac4f5aa5f976d4
SabiD76 <107200435+SabiD76@users.noreply.github.com>
2022-07-01 00:38:15 -1100

    Create emoction.py
    first commit

---
+++ emoction.py
@@ -0,0 +1,11 @@
+from textblob import TextBlob
+
+feedback1="The boy is good"
+feedback2="you are so bad"
+
+blob=TextBlob(feedback1)
+
+blob2= TextBlob(feedback2)
+
+print(blob.sentiment) 
+print(blob2.sentiment)
